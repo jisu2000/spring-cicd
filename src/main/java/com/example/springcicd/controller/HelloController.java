cf185af9502b6aeae34adc3179d42de238618c22
jisu2000 <spjisu2000@gmail.com>
2024-04-01 21:36:19 +0530

    buildspec modified

--- src/main/java/com/example/springcicd/controller/HelloController.java
+++ src/main/java/com/example/springcicd/controller/HelloController.java
@@ -1,1 +1,21 @@
-package com.example.springcicd.controller;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RestController;@RestController@RequestMapping("/api/v1")public class HelloController {    @GetMapping("/hello")    public String getHello(){        return "Hello World";    }    @GetMapping("/test")    public String test(){        return "Healthy";    }}
+package com.example.springcicd.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello(){
+        return "Hello World v2";
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "Healthy";
+    }
+}
